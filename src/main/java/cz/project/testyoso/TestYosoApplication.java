61d4691f3fb6fde37d8e23e5de8dcfdaa6a70a71
Arsen290 <arsen290@ukr.net>
2023-11-27 16:35:27 +0100

    Initialize project in git

---
+++ src/main/java/cz/project/testyoso/TestYosoApplication.java
@@ -0,0 +1,13 @@
+package cz.project.testyoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestYosoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestYosoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/project/testyoso/TestYosoApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.project.testyoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestYosoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
