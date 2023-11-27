fb479e8a9e4a186e863a8fc514785672a745e1a0
Arsen290 <arsen290@ukr.net>
2023-11-27 16:42:51 +0100

    Create entity User

---
+++ src/main/java/cz/project/testyoso/entities/User.java
@@ -0,0 +1,60 @@
+package cz.project.testyoso.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String givenName;
+    private String lastName;
+
+    public User(String username, String password, String givenName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.givenName = givenName;
+        this.lastName = lastName;
+    }
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+
+}
