e15f748e1bd7a84b6f7a9fdf8ef2328a26b003a6
Arsen290 <arsen290@ukr.net>
2023-11-27 17:16:53 +0100

    Create methods to create and get all users

--- src/main/java/cz/project/testyoso/repositories/UserRepository.java
+++ src/main/java/cz/project/testyoso/repositories/UserRepository.java
@@ -3,8 +3,10 @@ package cz.project.testyoso.repositories;
 import cz.project.testyoso.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
+    List<User> findAll();
 }
--- src/main/java/cz/project/testyoso/services/UserService.java
+++ src/main/java/cz/project/testyoso/services/UserService.java
@@ -4,6 +4,8 @@ import cz.project.testyoso.DTO.UserDTO;
 import cz.project.testyoso.entities.User;
 import cz.project.testyoso.repositories.UserRepository;
 
+import java.util.List;
+
 public class UserService {
     private final UserRepository userRepository;
     public UserService(UserRepository userRepository) {
@@ -16,6 +18,26 @@ public class UserService {
         userDTO.setGivenName(user.getGivenName());
         userDTO.setLastName(user.getLastName());
         return userDTO;
+    }
+
+    public void createUser(UserDTO userDTO) {
+            User user = new User();
+            user.setUsername(userDTO.getUsername());
+            user.setPassword(userDTO.getPassword());
+            user.setGivenName(userDTO.getGivenName());
+            user.setLastName(userDTO.getLastName());
+            userRepository.save(user);
+        }
+
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(this::toDTO).toList();
+    }
+
+    //implement to further work with data from specific user
+    public UserDTO getUserByUsername(String username) {
+        User user = userRepository.findUserByUsername(username).orElseThrow();
+        return toDTO(user);
     }
 
 
